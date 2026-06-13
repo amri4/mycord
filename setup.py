0afe217a35d59b90001d2179f249f3baf836f58b
amri4 <amri.supreme123@gmail.com>
2026-06-13 06:48:56 +0300

    Bump version from 1.0.8 to 1.0.9

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class CustomInstallCommand(install):
 # ==============================================================================
 setup(
     name="mycord",
-    version="1.0.8",
+    version="1.0.9",
     description="The ultimate self-syncing Discord bot framework framework.",
     author="luffy",
     packages=["mycord"],  # Points to your inner folder containing __init__.py
