c1d2c86aca3b9c597a1147dabc544eaffc7a28fd
amri4 <amri.supreme123@gmail.com>
2026-09-01 17:37:11 +0300

    Update server URL in punksdb.py

--- mycord/punksdb.py
+++ mycord/punksdb.py
@@ -7,7 +7,7 @@ class PunksDB:
     def __init__(
         self,
         db_name="mycord_data.db",
-        server="http://us.monkey-network.xyz:5002"
+        server="http://node9.quaxly.com:25474"
     ):
 
         self.db_name = db_name
