a5e39869500b61008c4941d25f732ce7095502c8
amri4 <amri.supreme123@gmail.com>
2026-09-05 20:37:02 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class CustomInstallCommand(install):
 # ==============================================================================
 setup(
     name="mycord",
-    version="1.2.4",
+    version="1.2.5",
     description="The ultimate self-syncing Discord bot framework framework.",
     author="luffy",
     packages=["mycord"],  # Points to your inner folder containing __init__.py
